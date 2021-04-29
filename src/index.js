dc54f4c75d2852d33ff51ed67ed8ac6318d8dbfb
Manuel Canadas <nanocanadas@gmail.com>
2021-04-29 19:02:05 +1200

    Board generating function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import "./components/Board";
+import "./components/Board.css"
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
